9d09cc9cf10343ea92959a9ad94c2278046d1919
Makss2022 <maximkhersonua@gmail.com>
2022-09-10 12:53:02 +0300

    design fixes

--- note.py
+++ note.py
@@ -1,8 +1,8 @@
 
 class Note:
-    def __init__(self, note: str, tags: list[str] = []) -> None:
-        self.note = note
+    def __init__(self, text: str, tags: list[str] = []) -> None:
+        self.text = text
         self.tags = tags
 
     def __repr__(self) -> str:
-        return "\nTegs: " + ", ".join([teg for teg in self.tegs]) + f"\nNote text: {self.note}"
+        return "\nTegs: " + ", ".join([tag for tag in self.tags]) + f"\nNote text: {self.note}"
