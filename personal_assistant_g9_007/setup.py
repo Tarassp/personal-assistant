ed8a468211139efb25563a2f27c853b001650d4d
taras <spasibovtaras@gmail.com>
2022-09-15 17:18:50 +0300

    update version to 2

--- personal_assistant_g9_007/setup.py
+++ personal_assistant_g9_007/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='personal_assistant_g9_007',
-    version='0.0.1',
+    version='0.0.2',
     description='Personal Assistant can help you organize your Contacts, Notes and Files',
     author='Team-lead Taras Spasibov, project group 9',
     author_email='spasibovtaras@gmail.com',
